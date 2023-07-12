8d2982d659f2df53306b2bee3117508fde7183e9
Aji Saputro <ajisapuutro.s.kom@gmail.com>
2023-07-12 20:36:38 +0700

    add Handler get all book

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-type User struct {
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type Book struct {
 	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = map[int]Book{
+	1: Book{ID: 1, Title: "Book 1", Author: "Author 1"},
+	2: Book{ID: 2, Title: "Book 2", Author: "Author 2"},
+}
+
+func getAllBooks(c echo.Context) error {
+	return c.JSON(http.StatusOK, books)
 }
